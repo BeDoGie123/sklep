762a96373cd1f6004440ddc6a7e5a290d987f4a1
BeDoGie123 <bartekggzk@gmail.com>
2026-03-23 09:35:11 +0100

    Update sklep.php

--- sklep.php
+++ sklep.php
@@ -18,6 +18,10 @@
     </header>
 
     <main>
+        <?php 
+            $conn = mysqli_connect('localhost','root','','sklep');
+            
+        ?>
         <aside id="leftMain"></aside>
         <section id="centerMain">
             <section id="loginSection">
@@ -28,19 +32,51 @@
                     <input type="submit" value="Zaloguj" id="loginButton">
                     <sub><em onclick="createUser()">Utwórz konto</em></sub>
                 </form>
+                <?php
+                    $l="";
+                    $h="";
+                    if(isset($_POST["login"])&&isset($_POST["password"])){
+                        $l=$_POST["login"];
+                        $h=sha1($_POST["password"]);
+                        $query="SELECT login,haslo FROM login WHERE login=$l AND haslo=$h";
+                        $result=mysqli_query($conn,$query);
+                        if(mysqli_num_rows($result)==0){
+                            echo "zalogowano pomyślnie";
+                        }
+                        else{
+                            echo "złe dane";
+                        }
+                    }  
+                ?>
             </section>
             <section id="createUserSection">
                 <p>Utwórz konto</p><br />
                 <form action="sklep.php" method="POST">
                     login<br /><input type="text" name="login" id="loginCreated"><br /><br />
                     hasło<br /><input type="password" name="password" id="passwordFirst"><br />
-                    powtórz hasło<input type="password" name="password" id="passwordSecond"><br /><br />
+                    powtórz hasło<input type="password" name="password2" id="passwordSecond"><br /><br />
                     <input type="submit" value="Utwórz konto" id="createUserButton">
                     <sub><em onclick="login()">Zaloguj się</em></sub>
                 </form>
+                <?php
+                    $login = $_POST['login'] ?? '';
+                    $haslo1 = $_POST['password'] ?? '';
+                    $haslo2 = $_POST['password2'] ?? '';
+                    $sql = "INSERT INTO loginy (login, haslo) values ('$login', sha1('$haslo1'))";
+                    if($haslo1 == $haslo2){
+                        mysqli_query($conn, $sql);
+                    }
+                    else{
+                        echo "Hasła sie różnią";
+                    }
+                    
+                ?>
             </section>
         </section>
         <aside id="rightMain"></aside>
+        <?php
+            mysqli_close($conn);
+        ?>
     </main>
 
     <footer>
@@ -64,19 +100,5 @@
             document.getElementsByTagName("button")[0].style.display="none";
         }
     </script>
-    <?php
-    $con = mysqli_connect('localhost','root','','sklep');
-    $login = $_POST['login'] ?? '';
-    $haslo1 = $_POST['password'] ?? '';
-    $haslo2 = $_POST['password2'] ?? '';
-    $sql = "INSERT INTO loginy (login, haslo) values ('$login', sha1('$haslo1'))";
-    if($haslo1 == $haslo2){
-        mysqli_query($con, $sql);
-    }
-    else{
-        echo "Hasła sie różnią";
-    }
-    mysqli_close($con);
-    ?>
 </body>
 </html>
