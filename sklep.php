632806a5e5ca5209684f5cc574db1d3ee73c4285
BeDoGie123 <bartekggzk@gmail.com>
2026-03-23 08:04:31 +0100

    Update sklep.php
    bloki logowania

--- sklep.php
+++ sklep.php
@@ -13,13 +13,30 @@
             <img src="shopping-store.png" alt="">
         </aside>
         <aside id="rightHeader">
-            <button onclick="login()"></button>
+            <button onclick="login()">Zaloguj się</button>
         </aside>
     </header>
 
     <main>
         <aside id="leftMain"></aside>
-        <section id="centerMain"></section>
+        <section id="centerMain">
+            <section id="loginSection">
+                <form action="sklep.php" method="POST">
+                    login<input type="text" name="login" id="login"><br /><br />
+                    hasło<input type="password" name="password" id="passwordLogin">
+                    <input type="submit" value="Utwórz konto">
+                </form>
+            </section>
+            <section id="createUserSection">
+                <p>Utwórz konto</p><br />
+                <form action="sklep.php" method="POST">
+                    login<input type="text" name="login" id="login"><br /><br />
+                    hasło<input type="password" name="password" id="passwordFirst"><br />
+                    powtórz hasło<input type="password" name="password" id="passwordSecond">
+                    <input type="submit" value="Utwórz konto">
+                </form>
+            </section>
+        </section>
         <aside id="rightMain"></aside>
     </main>
 
