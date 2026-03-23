c8e0aa708d3e02de33ec0132daa19d04ff9edd4e
BeDoGie123 <bartekggzk@gmail.com>
2026-03-23 09:06:17 +0100

    Update sklep.php

--- sklep.php
+++ sklep.php
@@ -10,7 +10,7 @@
 <body>
     <header>
         <aside id="leftHeader">
-            <img src="shopping-store.png" alt="">
+            <img src="shopping-store.png" alt="ikonka sklepu" onclick="main()">
         </aside>
         <aside id="rightHeader">
             <button onclick="login()">Zaloguj się</button>
@@ -21,19 +21,22 @@
         <aside id="leftMain"></aside>
         <section id="centerMain">
             <section id="loginSection">
+                <p>Zaloguj się</p><br />
                 <form action="sklep.php" method="POST">
-                    login<input type="text" name="login" id="login"><br /><br />
-                    hasło<input type="password" name="password" id="passwordLogin">
-                    <input type="submit" value="Utwórz konto">
+                    login<br /><input type="text" name="login" id="login"><br /><br />
+                    hasło<br /><input type="password" name="password" id="passwordLogin"><br /><br />
+                    <input type="submit" value="Zaloguj" id="loginButton">
+                    <sub><em onclick="createUser()">Utwórz konto</em></sub>
                 </form>
             </section>
             <section id="createUserSection">
                 <p>Utwórz konto</p><br />
                 <form action="sklep.php" method="POST">
-                    login<input type="text" name="login" id="login"><br /><br />
-                    hasło<input type="password" name="password" id="passwordFirst"><br />
-                    powtórz hasło<input type="password" name="password" id="passwordSecond">
-                    <input type="submit" value="Utwórz konto">
+                    login<br /><input type="text" name="login" id="loginCreated"><br /><br />
+                    hasło<br /><input type="password" name="password" id="passwordFirst"><br />
+                    powtórz hasło<input type="password" name="password" id="passwordSecond"><br /><br />
+                    <input type="submit" value="Utwórz konto" id="createUserButton">
+                    <sub><em onclick="login()">Zaloguj się</em></sub>
                 </form>
             </section>
         </section>
@@ -44,23 +47,21 @@
 
     </footer>
 
-        <?php
-    $con = mysqli_connect('localhost','root','','sklep');
-    $login = $_POST['login'] ?? '';
-    $haslo1 = $_POST['password'] ?? '';
-    $haslo2 = $_POST['password2'] ?? '';
-    $sql = "INSERT INTO loginy (login, haslo) values ('$login', '$haslo1')";
-    if($haslo1 == $haslo2){
-        mysqli_query($con, $sql);
-    }
-    else{
-        echo "Hasła sie różnią";
-    }
-    mysqli_close($con);
-    ?>
     <script>
+        function createUser(){
+            document.getElementById('createUserSection').style.display="block";
+            document.getElementById('loginSection').style.display="none";
+        }
         function login(){
-            return 0;
+            document.getElementById('createUserSection').style.display="none";
+            document.getElementById('loginSection').style.display="block";
+        }
+        function main(){
+            document.getElementById('createUserSection').style.display="none";
+            document.getElementById('loginSection').style.display="none";
+        }
+        function logged(){
+            document.getElementsByTagName("button")[0].style.display="none";
         }
     </script>
 </body>
