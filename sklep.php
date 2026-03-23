76e521400906326a86ddc0c762b48ba9a03c7422
BeDoGie123 <bartekggzk@gmail.com>
2026-03-23 07:27:33 +0100

    pliki

---
+++ sklep.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep spożywczy</title> 
+    <link rel="icon" type="image/png" href="store.png">
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <aside id="leftHeader">
+            <img src="shopping-store.png" alt="">
+        </aside>
+        <aside id="rightHeader">
+            <button onclick="login()"></button>
+        </aside>
+    </header>
+
+    <main>
+        <aside id="leftMain"></aside>
+        <section id="centerMain"></section>
+        <aside id="rightMain"></aside>
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script>
+        function login(){
+            return 0;
+        }
+    </script>
+</body>
+</html>
